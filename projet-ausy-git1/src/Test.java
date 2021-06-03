efdbcaaa9bdde79ec36c241e5ad07c2a2a36b846
vandelle aldric <aldric.v@club-internet.fr>
2021-06-03 13:54:57 +0200

    v3 p1

--- projet-ausy-git1/src/Test.java
+++ projet-ausy-git1/src/Test.java
@@ -5,6 +5,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Projet ausy git 1 version 1");
 		System.out.println("Projet ausy git 1 version 2");
+		System.out.println("Projet ausy git 1 version 3");
 	}
 
 }
