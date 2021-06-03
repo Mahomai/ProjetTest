5772acf778e24778837109609f23cd57773fb0e7
vandelle aldric <aldric.v@club-internet.fr>
2021-06-03 15:52:36 +0200

    v2

--- projet-ausy-gitest/src/Test.java
+++ projet-ausy-gitest/src/Test.java
@@ -4,6 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Projet ausy git Test version 1");
+		System.out.println("Projet ausy git Test version 1");
+		
 	}
 
 }
