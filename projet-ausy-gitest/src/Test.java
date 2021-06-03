2de0765625c637971855c17de815c7990fc25461
BenR57 <castitatis@outlook.fr>
2021-06-03 16:17:52 +0200

    v4

--- projet-ausy-gitest/src/Test.java
+++ projet-ausy-gitest/src/Test.java
@@ -6,6 +6,8 @@ public class Test {
 		System.out.println("Projet ausy git Test version 1");
 		System.out.println("Projet ausy git Test version 2");
 		System.out.println("Projet ausy git Test version 3");
+
+		System.out.println("Projet ausy git Test version 4");
 	}
 
 }
